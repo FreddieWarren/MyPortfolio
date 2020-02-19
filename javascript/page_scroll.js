5451e16f3aad05fe7b7d66ef09be0aaa843811b2
freddiewarren <fredswow@live.co.uk>
2020-02-19 18:32:03 +0000

    restarted portfolio focusing on responsiveness

--- javascript/page_scroll.js
+++ javascript/page_scroll.js
@@ -3,3 +3,29 @@ $("#about-btn").click(function() {
         scrollTop: $("#about").offset().top
     }, 1000);
 });
+
+// $(window).bind('scroll', function () {
+//     var menu = $('.nav-menu');
+//     if ($(window).scrollTop() > menu.offset().top) {
+//         menu.addClass('fixed');
+//     } else {
+//         menu.removeClass('fixed');
+//     }
+// });
+
+
+$("document").ready(function($){
+    var nav = $('#nav');
+    var height =$('#home').height();
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > height - 50){
+            nav.addClass("fixed");
+        } else {
+            nav.removeClass("fixed");
+        }
+    });
+});
+
+
+
