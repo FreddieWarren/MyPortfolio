0145eeaa3ac0ac5275563328913be5d6b7ec6ccc
freddiewarren <fredswow@live.co.uk>
2020-08-21 16:53:16 +0100

    changed tab content style completely, making it cleaner/smarter/modern

--- javascript/tabs.js
+++ javascript/tabs.js
@@ -1,19 +1,19 @@
-var tabButtons=document.querySelectorAll("button");
-var tabPanels=document.querySelectorAll(".tab-panel");
+var tabButtons = document.querySelectorAll("button");
+var tabPanels = document.querySelectorAll(".tab-panel");
 
 function showPanel(panelIndex, colorCode) {
-  tabButtons.forEach(function(node){
-    node.style.backgroundColor="";
-    node.style.cColor="";
+  tabButtons.forEach(function (node) {
+    node.style.backgroundColor = "";
+    node.style.cColor = "";
   });
-  tabButtons[panelIndex].style.backgroundColor=colorCode;
-  tabButtons[panelIndex].style.color="white";
-  tabPanels.forEach(function(node){
-    node.style.display="none";
+  tabButtons[panelIndex].style.backgroundColor = colorCode;
+  tabButtons[panelIndex].style.color = "#616161";
+  tabPanels.forEach(function (node) {
+    node.style.display = "none";
   });
-  tabPanels[panelIndex].style.display="block";
-  tabPanels[panelIndex].style.backgroundColor=colorCode;
+  tabPanels[panelIndex].style.display = "block";
+  tabPanels[panelIndex].style.backgroundColor = colorCode;
 }
 
 // showPanel(0, '#EA785B');
-showPanel(0, 'var(--secondary-color)');
+showPanel(0, 'white)');
