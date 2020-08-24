3d568b65040bb542b8ad03b79b52f31e52454ffa
freddiewarren <fredswow@live.co.uk>
2020-08-24 14:01:48 +0100

    changed project tab button style. changed style of contact panel.

--- javascript/tabs.js
+++ javascript/tabs.js
@@ -4,15 +4,20 @@ var tabPanels = document.querySelectorAll(".tab-panel");
 function showPanel(panelIndex, colorCode) {
   tabButtons.forEach(function (node) {
     node.style.backgroundColor = "";
-    node.style.cColor = "";
+    node.style.color = "grey";
+    node.style.textDecoration = "none"
   });
   tabButtons[panelIndex].style.backgroundColor = colorCode;
   tabButtons[panelIndex].style.color = "#616161";
+  tabButtons[panelIndex].style.textDecoration = "underline";
   tabPanels.forEach(function (node) {
     node.style.display = "none";
+    node.style.color = "blue";
   });
   tabPanels[panelIndex].style.display = "block";
   tabPanels[panelIndex].style.backgroundColor = colorCode;
+
+
 }
 
 // showPanel(0, '#EA785B');
